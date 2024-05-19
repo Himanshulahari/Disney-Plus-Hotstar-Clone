d18b2d75aa66a4323a5eec35b9e4e3482e1c35a5
Himanshulahari <130894582+Himanshulahari@users.noreply.github.com>
2024-05-19 11:21:39 +0530

    added data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Frozen',
+        des: 'Fearless Anna teams up with Kristoff and a snowman named Olaf to find her sister Elsa, whose icy powers have trapped the kingdom in eternal winter.',
+        image: 'images/Frozen.jpg'
+    },
+    {
+        name: 'Shang-Chi and the legend of the ten rings',
+        des: 'Shang-Chi, a martial arts master, must confront the past the thought he left behind when he is drawn into the web of the mysterious Ten Rings organization.',
+        image: 'images/Shang-Chi.jpg'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Wanda Maximoff and Vision-two super-powered beings living idealized suburban lives-begin to suspend that everything is not as it seems.',
+        image: 'images/Wanda.jpg'
+    },
+    {
+        name: 'the lion king',
+        des: 'In the African savanna, a young prince overcomes betrayal and tradegy to assume his rightful place on Pride Rock.',
+        image: 'images/thelionking.jpg'
+    },
+    {
+        name: 'luca',
+        des: 'The movie is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.',
+        image: 'images/luca.jpg'
+    }
+]
